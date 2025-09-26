b312aefd750fd202c0f07284c760c076c19626c6
shawn <shawnhutchins1@gmail.com>
2025-09-25 21:28:47 -0400

    Opened the weather_data.csv with pandas

--- main.py
+++ main.py
@@ -3,14 +3,20 @@
 #   for index in range(len(data)):
 #        data[index] = data[index].strip("\n")
 
-import csv
+# import csv
+#
+# TEMP_COL = 1
+#
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperatures = []
+#     #skipping the first row of the data
+#     next(data)
+#     for row in data:
+#         temperatures.append(int(row[TEMP_COL]))
+#     print(temperatures)
 
-TEMP_COL = 1
+import pandas
 
-with open("weather_data.csv") as data_file:
-    data = csv.reader(data_file)
-    temperatures = []
-    next(data)
-    for row in data:
-        temperatures.append(int(row[TEMP_COL]))
-    print(temperatures)
+data = pandas.read_csv("weather_data.csv")
+print(data)
