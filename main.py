754ac3c7d377549e68edcee17a70cb65b8f3b992
shawn <shawnhutchins1@gmail.com>
2025-09-28 21:37:08 -0400

    Getting data in rows

--- main.py
+++ main.py
@@ -31,5 +31,15 @@ temps_list = temperatures.to_list()
 #print(data["temp"].max())
 
 #get data in columns
-print(data["condition"])
-print(data.condition)
+#print(data["condition"])
+#print(data.condition)
+
+#get data in rows
+#print(data[data.day == "Monday"])
+#print(data[data.temp == data.temp.max()])
+
+monday = data[data.day == "Monday"]
+monday_temp = monday.temp
+monday_temp_F = (monday_temp * 9/5) + 32
+print(f"monday temp C: {monday_temp}")
+print(monday_temp_F)
